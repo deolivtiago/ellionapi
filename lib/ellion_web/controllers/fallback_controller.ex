64cfc81c856b2a5ffe10069eb99e0b63e05acefa
Tiago de Oliveira <deoliv.tiago@gmail.com>
2023-10-02 20:20:50 -0300

    feat: add fallback controller for errors

---
+++ lib/ellion_web/controllers/fallback_controller.ex
@@ -0,0 +1,16 @@
+defmodule EllionWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use EllionWeb, :controller
+
+  @spec call(Plug.Conn.t(), {:error, Ecto.Changeset.t()}) :: Plug.Conn.t()
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(json: EllionWeb.ChangesetJSON)
+    |> render(:error, changeset: changeset)
+  end
+end
